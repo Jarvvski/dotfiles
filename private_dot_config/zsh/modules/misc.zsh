604724797f29dbcf3c13377ca93c65f12560561a
Adam Jarvis <adam@ameba.ai>
2026-07-21 11:25:53 +0100

    chezmoi sync

--- private_dot_config/zsh/modules/misc.zsh
+++ private_dot_config/zsh/modules/misc.zsh
@@ -14,6 +14,7 @@ export NODE_REPL_HISTORY="$XDG_STATE_HOME/node_repl_history"
 export KONAN_DATA_DIR="$XDG_DATA_HOME/konan"
 export GRADLE_USER_HOME="$XDG_DATA_HOME/gradle"
 export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
+export PI_CODING_AGENT_DIR="${PI_CODING_AGENT_DIR:-$XDG_CONFIG_HOME/pi}"
 export LS_COLORS="di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
 
 # zoxide exclusions
